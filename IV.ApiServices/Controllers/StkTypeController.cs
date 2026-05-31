3a9d28f9f4d40e91066de2f4f8139d6a4956985b
T8et <devt8et2022@gmail.com>
2026-05-31 23:11:45 +0630

    feat : crud normal flow

--- IV.ApiServices/Controllers/StkTypeController.cs
+++ IV.ApiServices/Controllers/StkTypeController.cs
@@ -1,6 +1,7 @@
 ﻿using IV.DataCenter.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace IV.ApiServices.Controllers
 {
@@ -16,5 +17,75 @@ namespace IV.ApiServices.Controllers
             var response = db.BtStkTypes.ToList();
             return Ok(response);
         }
+
+        [HttpGet("GetbyId")]
+        public IActionResult GetById(int id)
+        {
+            var response = db.BtStkTypes.Where(x=>x.StkTypeId==id).FirstOrDefault();
+            if(response is null) return BadRequest();
+            return Ok(response);
+        }
+
+        [HttpPost("newstktype")]
+        public IActionResult CreateStkType(BtStkType dataModel)
+        {
+            db.BtStkTypes.Add(dataModel);
+            db.SaveChanges();
+            return Ok("Inserted Successfully");
+        }
+
+        [HttpPatch("PatchUpdate")]
+        public IActionResult PatchUpdate(int id, BtStkType dataModel)
+        {
+            var list = db.BtStkTypes.Where(x => x.StkTypeId == id).FirstOrDefault();
+            if (list is null)
+            {
+                return BadRequest();
+            }
+
+            if(dataModel.StkTypeName != null) list.StkTypeName = dataModel.StkTypeName;
+            if (dataModel.StkTypeDesc != null) list.StkTypeDesc = dataModel.StkTypeDesc;
+            if (dataModel.TimeLog != null) list.TimeLog = dataModel.TimeLog;
+            if (dataModel.UserLog != null) list.UserLog = dataModel.UserLog;
+
+            db.Entry(list).State = EntityState.Modified;
+            db.SaveChanges();
+            return Ok(list);
+        }
+
+        [HttpPut("PutUpdate")]
+        public IActionResult PutUpdate(int id,BtStkType dataModel)
+        {
+            var list = db.BtStkTypes.Where(x=>x.StkTypeId == id).FirstOrDefault();
+            if (list is null)
+            {
+                return BadRequest();
+            }
+
+            list.StkTypeName = dataModel.StkTypeName;
+            list.StkTypeDesc = dataModel.StkTypeDesc;
+            list.TimeLog = dataModel.TimeLog;
+            list.UserLog = dataModel.UserLog;
+
+            db.Entry(list).State = EntityState.Modified;
+            db.SaveChanges();
+            return Ok(list);
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult Delete(int id)
+        {
+            var list = db.BtStkTypes.Where(x=>x.StkTypeId==id).FirstOrDefault();
+
+            if(list is null)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(list).State = EntityState.Deleted;
+            db.SaveChanges();
+            return Ok("Deleted Successful");
+        }
+
     }
 }
